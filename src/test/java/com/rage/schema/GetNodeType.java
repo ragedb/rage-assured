a7a12abb70566dc2df62966cad20c69e967df101
Max De Marzi <maxdemarzi@hotmail.com>
2021-08-09 09:42:58 -0500

    working on node types

--- src/test/java/com/rage/schema/GetNodeType.java
+++ src/test/java/com/rage/schema/GetNodeType.java
@@ -1,14 +1,16 @@
 package com.rage.schema;
 
-import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
 import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.specification.*;
+import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class GetNodeTypes {
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.hasItem;
+
+public class GetNodeType {
 
     private static RequestSpecification requestSpec;
 
@@ -28,22 +30,21 @@ public class GetNodeTypes {
     }
 
     @Test
-    public void requestNodeTypesFromEmptyGraph() {
+    public void requestNodeTypeFromEmptyGraph() {
 
         given().
                 spec(requestSpec).
         when().
-                get("/db/rage/schema/nodes").
+                get("/db/rage/schema/nodes/User").
         then().
                 assertThat().
                 statusCode(200).
                 contentType(equalTo("application/json")).
-                body("", empty()).
-                body(equalTo("[]"));
+                body("$", not(hasItem("name")));
     }
 
     @Test
-    public void requestNodeTypesFromSingleNodeTypeGraph() {
+    public void requestEmptyNodeTypesFromSingleNodeTypeGraph() {
         given().
                 spec(requestSpec).
         when().
@@ -55,40 +56,48 @@ public class GetNodeTypes {
         given().
                 spec(requestSpec).
         when().
-                get("/db/rage/schema/nodes").
+                get("/db/rage/schema/nodes/User").
         then().
                 assertThat().
                 statusCode(200).
                 contentType(equalTo("application/json")).
-                body("", hasItem("User"));
+                body("", not(hasItem("name")));
     }
 
     @Test
-    public void requestNodeTypesFromMultipleNodeTypeGraph() {
+    public void requestNonEmptyNodeTypeFromSingleNodeTypeGraph() {
         given().
                 spec(requestSpec).
-                when().
+        when().
                 post("/db/rage/schema/nodes/User").
-                then().
+        then().
                 assertThat().
                 statusCode(201);
         given().
                 spec(requestSpec).
-                when().
-                post("/db/rage/schema/nodes/Person").
-                then().
+        when().
+                post("/db/rage/schema/nodes/User/properties/name/string").
+        then().
                 assertThat().
                 statusCode(201);
 
         given().
                 spec(requestSpec).
         when().
-                get("/db/rage/schema/nodes").
+                post("/db/rage/schema/nodes/User/properties/age/integer").
+        then().
+                assertThat().
+                statusCode(201);
+
+        given().
+                spec(requestSpec).
+        when().
+                get("/db/rage/schema/nodes/User").
         then().
                 assertThat().
                 statusCode(200).
                 contentType(equalTo("application/json")).
-                body("", hasItem("User")).
-                body("", hasItem("Person"));
+                body("age", is("integer")).
+                body("name", is("string"));
     }
 }
